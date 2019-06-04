7210b37203ba6f059b94ebca61b5c3d47b60eb3a
Guilherme <gbletsch@gmail.com>
2019-06-04 19:09:19 -0300

    Succesfully import clientes.csv and the files from estado de SP starting the CRUD

--- main.go
+++ main.go
@@ -2,105 +2,54 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
-	"strings"
+
+	_ "github.com/lib/pq"
 )
 
-var originalFilename = "remuneracao_Marco_2019.rar"
-var savedFilename = "Remuneracao_Marco_2019.txt"
-var httpAddress = "http://www.transparencia.sp.gov.br/PortalTransparencia-Report/historico"
-var extractFolder = "extracted"
-var downloadFolder = "downloaded"
-var downloadedFilePath = fmt.Sprintf("%v/%v", downloadFolder, originalFilename)
-var httpPath = fmt.Sprintf("%v/%v", httpAddress, originalFilename)
-var extractedFilePath = fmt.Sprintf("%v/%v", extractFolder, savedFilename)
-var dbaccountHoldersPath = "db/accountHolders.csv"
+const (
+	DB_USER     = "postgres"
+	DB_PASSWORD = "12345"
+	DB_NAME     = "postgres"
+)
 
-const indexaccountHolderName int = 0
-const indexisBankFunc int = 1
-const indexisPublicFunc int = 2
-const indexIncome int = 3
+type customer struct {
+	ID          int32   `json:"id"`
+	Name        string  `json:"name"`
+	Wage        float32 `json:"wage"`
+	IsPublic    int8    `json:"isPublic"`
+	SentWarning string  `json:"sentWarning"` // todo: colocar timestamp
+}
+
+type publicEmployee struct {
+	ID    int32   `json:"id"`
+	Name  string  `json:"name"`
+	Wage  float32 `json:"wage"`
+	Local string  `json:"local"`
+}
 
 func main() {
-	menuPrinc()
-}
+	// mainMenu()
+	// r, err := deleteAllRows("public_employees")
+	// fmt.Println(r, err)
+	// fmt.Println(importSPFile())
+	// db := openDB()
+	// defer db.Close()
 
-func menuPrinc() {
+	// createAllTables(db)
+	// fmt.Println(listTables())
+	// fmt.Println(dropTable("customers"))
 
-	fmt.Println("\nMenu inicial:\n*************")
-	fmt.Println("S-\tSetup novo banco de dados")
-	fmt.Println("I-\tImportar arquivo de clientes")
-	fmt.Println("A-\tAtualizar, comparando com lista dos funcionários públicos")
-	fmt.Println("C-\tCadastrar usuários para receber alertas")
-	fmt.Println("L-\tListar todos os clientes")
-	fmt.Println("U-\tListar usuários")
-	fmt.Println("D-\tDashboard")
+	// answer, err := readEmployee("WHERE wage > 20000")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// for _, a := range answer {
+	// 	fmt.Println(a)
+	// }
+	// fmt.Println(len(answer))
 
-	choice := readStringInput("\nDigite a opção desejada: ")
+	var e publicEmployee
+	e.read("WHERE name LIKE '%SOUZA%' ORDER BY RANDOM() LIMIT 1")
+	fmt.Println(e)
 
-	if choice == "I" {
-		err := importClientesCSV("clientes.csv")
-		if err != nil {
-			log.Fatal(err)
-		}
-		menuPrinc()
-	} else if choice == "U" {
-		err := readUsers()
-		if err != nil {
-			fmt.Printf("Could not read db: %v", err)
-		}
-		menuPrinc()
-	} else if choice == "L" {
-		err := readAccountHolders()
-		if err != nil {
-			fmt.Printf("Could not read db: %v", err)
-		}
-		menuPrinc()
-	} else if choice == "S" {
-		err := setupDB()
-		if err != nil {
-			log.Printf("Could not setup a new db: %v", err)
-		}
-		menuPrinc()
-	} else if choice == "C" {
-		addNewUser()
-		menuPrinc()
-	} else {
-		fmt.Println("\nFudeu!!!")
-	}
-
-}
-
-func addNewUser() {
-	var u user
-	u.Email = readStringInput("Digite o email do usuário: ")
-	u.Password = readStringInput("Digite a senha: ")
-	err := addUser(u)
-	if err != nil {
-		log.Printf("não foi possivel cadastrar usuário: %v", err)
-	}
-	again := readStringInput("\nCadastrar mais um? (s/n) ")
-	if again == "s" {
-		addNewUser()
-	}
-}
-
-func makeMapAgents() map[string]string {
-	var indexName int
-	var indexIncome = 3
-
-	if _, err := os.Stat(extractedFilePath); os.IsNotExist(err) {
-		downloadPublicAgentsFile(false)
-	}
-
-	mapAgents := make(map[string]string)
-	job := func(row []string) bool {
-		r := strings.Replace(row[indexIncome], ",", ".", 1)
-		mapAgents[row[indexName]] = r
-		return true
-	}
-
-	readCSV(extractedFilePath, job, ';', true)
-	return mapAgents
 }
