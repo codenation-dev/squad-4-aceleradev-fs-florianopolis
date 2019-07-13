25b5f4579d3e3b60931d60f18f20c6b919a36e7a
EDGutterres <diasgt.eduardo@gmail.com>
2019-07-13 15:05:03 -0300

    adding login screen

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Login = () => {
-    return (
-        <h1>Login</h1>
+class Login extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render = () => (
+        <form className="form-signin">
+            <div className="text-center mb-4">
+                <h1 className="h3 mb-3 font-weight-normal">Login / Register</h1>
+            </div>
+    
+            <div className="form-label-group">
+                <label htmlFor="inputEmail">Username</label>
+                <input
+                    name="username"
+                    onChange={() => {}}
+                    value={''}
+                    className="form-control"
+                    placeholder="Username"
+                    required
+                />
+            </div>
+    
+            <div className="form-label-group mt-2">
+                <label htmlFor="inputPassword">Password</label>
+                <input
+                    name="password"
+                    onChange={() => {}}
+                    value={''}
+                    type="password"
+                    className="form-control"
+                    placeholder="Password"
+                    required
+                />
+            </div>
+    
+            <div className="mt-5">
+                <button className="login btn btn-lg btn-primary btn-block" type="submit">Login</button>
+                <button className="register btn btn-lg btn-secondary btn-block" type="submit">Register</button>
+            </div>
+        </form>
     )
 }
 
