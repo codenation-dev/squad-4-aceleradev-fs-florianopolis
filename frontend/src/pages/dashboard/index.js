8668794e22ed6cf1789c83b7cfa8545adcf80f2f
jorgelohn <jglohn@gmail.com>
2019-07-13 16:01:28 -0300

    Login Service

--- frontend/src/pages/dashboard/index.js
+++ frontend/src/pages/dashboard/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Dashboard = () => {
+
+const Dashboard = () => {    
     return (
         <>
             <h1>Dashboard</h1>
