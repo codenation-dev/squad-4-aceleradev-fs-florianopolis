3a69f9818c6c051fdf1a7805e0cb8fcb4354d320
Guilherme <gbletsch@gmail.com>
2019-07-18 15:01:38 -0300

    first commit

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: squad-4-aceleradev-fs-florianopolis
--- backend/pkg/model/public_func.go
+++
@@ -1,9 +0,0 @@
-package model
-
-// PublicFunc models the public employee profile with relevant informations
-type PublicFunc struct {
-	ID    int     `json:"id"`
-	Name  string  `json:"name"`
-	Wage  float32 `json:"wage"`
-	Place string  `json:"place"` // Place of work
-}
