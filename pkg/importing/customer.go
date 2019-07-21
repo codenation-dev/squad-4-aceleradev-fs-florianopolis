183cba80358ecaa8a3c18ac1a0b6f14e45a08a5e
Guilherme <gbletsch@gmail.com>
2019-07-21 10:19:03 -0300

    import customer

--- pkg/importing/customer.go
+++ pkg/importing/customer.go
@@ -6,23 +6,16 @@ import (
 	"github.com/codenation-dev/squad-4-aceleradev-fs-florianopolis/pkg/entity"
 )
 
-// FetchCustomerData imports the data from clientes.csv file
-func FetchCustomerData(company string) ([]entity.Customer, error) {
-	if company == "uati" {
-		return fetchUatiData()
-	}
-	return nil, entity.ErrNotImplemented
-}
-
-func fetchUatiData() ([]entity.Customer, error) {
+func importCustomerData(path string) ([]entity.Customer, error) {
 	var customers []entity.Customer
-	path := "../cmd/data/downloaded/clientes.csv"
+	path = "../cmd/data/cache/clientes.csv"
 	job := func(row []string) bool {
 		customers = append(customers, entity.Customer{
 			Name: row[0],
 		})
 		return true
 	}
+
 	readCSV(path, job, ',', true)
 	fmt.Println(customers)
 	return customers, nil
